36e202ebcd651ddd5022c1a05ad680cadf5a887e
necrosilencer <62260220+necrosilencer@users.noreply.github.com>
2020-03-16 19:35:40 -0300

    Add files via upload

---
+++ form.php
@@ -0,0 +1,21 @@
+<?php 
+ include ("principal.html");
+ ?>
+
+
+ <h1>Formularío </h1>
+ <form action="">
+ 	<label for="">Nome</label>
+ 	<input type="text">
+ 	<br>
+ 	<label for="">Sobrenome</label>
+ 	<input type="text">
+ 	<br>
+ 	<label for="">Endereço</label>
+ 	<input type="text">
+ 	<br>
+
+ 	<input type="submit" value="enviar">
+ 	<input type="reset" value="limpar">
+
+ </form>
